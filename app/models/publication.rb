a24ed40b1e6cf536f536edac1d0ca8229cca2d53
Pihla Toivanen <toivanenpihla@gmail.com>
2015-04-07 16:14:53 +0300

    Publicationiin lisätty has_many tyypit

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,3 +1,9 @@
 class Publication < ActiveRecord::Base
   has_many :articles, through: :publication_articles
+  has_many :books, through: :publication_books
+  has_many :booklets, through: :publication_booklets
+  has_many :inbooks, through: :publication_inbooks
+  has_many :inproceedings, through: :publication_inproceedings
+  has_many :manuals, through: :publication_manuals
+  has_many :masterthesis, through: :masterthesis
 end
