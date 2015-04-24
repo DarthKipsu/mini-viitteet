1f4e334f8df6055cf3b2fe9579dde1a433d45faa
Pihla Toivanen <toivanenpihla@gmail.com>
2015-04-24 13:26:52 +0300

    Achievementin imagelink ja achievementin sulkeminen

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421130959) do
+ActiveRecord::Schema.define(version: 20150424091630) do
 
   create_table "achievements", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "imagelink"
   end
 
   create_table "articles", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-Achievement.create name: 'Matti Luukkainen', description: 'Added Matti Luukkainen as a reference'
+Achievement.create name: 'Matti Luukkainen', description: 'Added Matti Luukkainen as a reference', imagelink: "https://www.cs.helsinki.fi/sites/default/files/pictures/mattliluennoi.png"
